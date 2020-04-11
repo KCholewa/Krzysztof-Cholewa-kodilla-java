e103cd5787b8d6c1b28410113fe05e5da593d490
kcholewa81 <k.cholewa81@gmail.com>
2020-04-11 20:28:47 +0100

    Zadanie 7.2

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,19 @@
+package com.kodilla.stream.immutable;
+
+public final class ForumUser {
+    private final String usermame;
+    private final String realName;
+
+    public ForumUser(final String usermame, final String realName) {
+        this.usermame = usermame;
+        this.realName = realName;
+    }
+
+    public String getUsermame() {
+        return usermame;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+}
