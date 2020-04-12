bca85099804cebdeeda40bd4d9361332ccaa5ec8
kcholewa81 <k.cholewa81@gmail.com>
2020-04-12 16:39:38 +0100

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -3,7 +3,6 @@ package com.kodilla.stream.immutable;
 public final class ForumUser {
     private final String usermame;
     private final String realName;
-    int a;
 
     public ForumUser(final String usermame, final String realName) {
         this.usermame = usermame;
