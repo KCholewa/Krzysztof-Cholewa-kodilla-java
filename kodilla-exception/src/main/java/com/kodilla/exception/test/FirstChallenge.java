376a34750d854ef1a920009b109afc26d4e84a16
kcholewa81 <k.cholewa81@gmail.com>
2020-04-21 15:55:27 +0100

    Zadanie8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,21 +2,13 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-     public double divide(double a, double b) throws ArithmeticException {
 
-        double product = 0;
-        try {
-             product = a / b;
-             if(b == 0){
-                  throw new ArithmeticException();
-             }
-        } catch (ArithmeticException e) {
-             System.out.println("Division by Zero: "+e);
-        } finally {
-             System.out.println("Division attempt done");
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
         }
-        return product;
-     }
+        return a / b;
+    }
 
         /**
          * This main can throw an ArithmeticException!!!
@@ -27,9 +19,14 @@ public class FirstChallenge {
 
         FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3, 0);
-
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Division by Zero: "+e);
+        } finally {
+            System.out.println("Division attempt done");
+        }
 
     }
 
