2c356919d13b1745cb509251c590ba4b09dea164
kcholewa81 <k.cholewa81@gmail.com>
2020-04-29 17:04:21 +0100

    Zadanie 13.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/shopping/OrderFoodRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/shopping/OrderFoodRepository.java
@@ -1,10 +1,10 @@
 package com.kodilla.good.patterns.challenges.shopping;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class OrderFoodRepository implements ShoppingRepository {
 
-    private ArrayList<Order> orderFoodRepository = new ArrayList<>();
+    private List<Order> orderFoodRepository = new ArrayList<>();
 
     public void addToRepository(Order order) {
         orderFoodRepository.add(order);
