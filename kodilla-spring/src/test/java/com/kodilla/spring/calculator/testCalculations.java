0a1c99d1ae7f3bd4c525769a7128dcc8c31d6495
kcholewa81 <k.cholewa81@gmail.com>
2020-05-10 22:22:04 +0100

    [14.2]

--- kodilla-spring/src/test/java/com/kodilla/spring/calculator/testCalculations.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/testCalculations.java
@@ -20,16 +20,17 @@ public class testCalculations {
         Calculator calculator = context.getBean(Calculator.class);
 
         //when
+
         double sum = calculator.add(10,5);
         double sub = calculator.sub(10, 5);
         double mul = calculator.mul(10,5);
         double div = calculator.div(10, 5);
 
         //then
+        Assert.assertEquals(true, Double.isFinite(div));
         Assert.assertEquals(15.0, sum, 0.01);
         Assert.assertEquals(5.0, sub, 0.01);
         Assert.assertEquals(50.0, mul, 0.01);
         Assert.assertEquals(2.0, div, 0.01);
-
     }
 }
