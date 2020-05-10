b3c4747efdd027ad8c7e9868ec71d19a5b7922bd
kcholewa81 <k.cholewa81@gmail.com>
2020-05-10 13:49:30 +0100

    [14.2]

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/testCalculations.java
@@ -0,0 +1,35 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class testCalculations {
+
+    @Test
+    public void testCalculatorFunctions() {
+        //given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+
+        //when
+        double sum = calculator.add(10,5);
+        double sub = calculator.sub(10, 5);
+        double mul = calculator.mul(10,5);
+        double div = calculator.div(10, 5);
+
+        //then
+        Assert.assertEquals(15.0, sum, 0.01);
+        Assert.assertEquals(5.0, sub, 0.01);
+        Assert.assertEquals(50.0, mul, 0.01);
+        Assert.assertEquals(2.0, div, 0.01);
+
+    }
+}
